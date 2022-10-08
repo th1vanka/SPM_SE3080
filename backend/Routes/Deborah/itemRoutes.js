4316cdd9d8ccafb60667bec037bc82c82b987c18
IT20026448 <it20026448@my.sliit.lk>
2022-10-08 22:01:13 +0530

    debicommit

---
+++ backend/Routes/Deborah/itemRoutes.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const cloudinary = require("../../utils/cloudinary");
+const Items = require("../../Models/Deborah/items");
+const router = express.Router();
+
+//to save item details
+router.post("/items/save", (req, res) => {
+  const paths = req.file.path;
+  const Image = req.body;
+  const Images = new Items({Image:""});
+
+  cloudinary.uploader.upload(paths, function (error, result) {
+    if (error) {
+      console.log(error);
+    } else {
+      let link = result.secure_url;
+      console.log(link);
+
+      Images.save()
+      .then((data)=>{
+          res.json(data)
+          const image = new Items({Image : link })
+          image.save()
+      })
+      .catch(err=>{
+          res.json(err)
+      })
+    }
+  });
+
+  //   function SaveData() {
+  //     newItem.save((err) => {
+  //       if (err) {
+  //         return res.status(400).json({
+  //           error: err,
+  //         });
+  //       } else {
+  //         return res.status(200).json({
+  //           success: "Item has been added successfully",
+  //         });
+  //       }
+  //     });
+  //   }
+});
+
+//read all item details from database
+router.get("/getItemDetails", (req, res) => {
+  Items.find().exec((err, getAllItems) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingItems: getAllItems,
+    });
+  });
+});
+
+//get specific item's details
+router.get("/getOneItem/:id", (req, res) => {
+  let itemID = req.params.id;
+
+  Items.findById(itemID, (err, getOneItem) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        err,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      getOneItem,
+    });
+  });
+});
+
+//update item details
+router.put("/itemDetails/update/:id", (req, res) => {
+  Items.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, itemDetails) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+
+      return res.status(200).json({
+        success: "updated",
+      });
+    }
+  );
+});
+
+//delete specific item's details
+router.delete("/itemDetails/delete/:id", (req, res) => {
+  Items.findByIdAndRemove(req.params.id).exec((err, deletedItemDetails) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Delete unsuccessful",
+        err,
+      });
+    }
+
+    return res.json({
+      message: "Delete successful",
+      deletedItemDetails,
+    });
+  });
+});
+
+module.exports = router;
